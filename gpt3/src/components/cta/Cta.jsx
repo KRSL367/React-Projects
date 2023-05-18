a950bb4f3419e0f9a1804997f6de3d302bbddaf3
KRSL367 <krishalpudasaini12345@gmail.com>
2023-05-18 18:45:47 +0545

    CTA_Build

--- gpt3/src/components/cta/Cta.jsx
+++ gpt3/src/components/cta/Cta.jsx
@@ -3,7 +3,14 @@ import './cta.css'
 
 const Cta = () => {
   return (
-    <div>
+    <div className='gpt3__cta'>
+      <div className='gpt3__cta-content'>
+        <p>Don't miss your chance and Learn Now</p>
+        <h3>Start Learning today and have endless possibilities</h3>
+      </div>
+      <div className='gpt3__cta-button'>
+        <button type='button'>Get Started</button>
+      </div>
       
     </div>
   )
