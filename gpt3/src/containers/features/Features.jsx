9cd15495dc3836d4137e14a6119139abc3c8dcae
KRSL367 <krishalpudasaini12345@gmail.com>
2023-05-18 17:15:36 +0545

    React Project_1.5

--- gpt3/src/containers/features/Features.jsx
+++ gpt3/src/containers/features/Features.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
 import './features.css'
+import { Feature } from '../../components'
+
+const featuresData = [
+  {
+    title: 'Do your Best',
+    text: 'Welcome to the ultimate course, where you will learn how to take Figma designs, turn them into four modern, responsive, beautifully-designed applications'
+  },
+  {
+    title: 'Do your Best',
+    text: 'Welcome to the ultimate course, where you will learn how to take Figma designs, turn them into four modern, responsive, beautifully-designed applications'  },
+  {
+    title: 'Do your Best',
+    text: 'Welcome to the ultimate course, where you will learn how to take Figma designs, turn them into four modern, responsive, beautifully-designed applications'  },
+  {
+    title: 'Do your Best',
+    text: 'Welcome to the ultimate course, where you will learn how to take Figma designs, turn them into four modern, responsive, beautifully-designed applications'  }
+]
 
 const Features = () => {
   return (
-    <div>
-      Hello
+    <div className='gpt3__features section__padding' id='features'>
+      <div className='gpt3__features-heading'>
+        <h1 className='gradient__text'>Need to learn alot about React and is fun learning React</h1>
+        <p>Don't miss your chance and Learn Now</p>
+      </div>
+      <div className='gpt3__features-container'>
+        {featuresData.map((item,index)=> (
+          <Feature title={item.title} text={item.text} key={item.title + index}/>
+        ))}
+
+      </div>
+      
     </div>
   )
 }
