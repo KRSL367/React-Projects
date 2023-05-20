5a0f5e0bf65ff4840ecfdef79a55daec91fd4bb5
KRSL367 <krishalpudasaini12345@gmail.com>
2023-05-20 16:51:51 +0545

    Footer Completed

--- gpt3/src/containers/footer/Footer.jsx
+++ gpt3/src/containers/footer/Footer.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
 import './footer.css';
+import gpt3Logo from '../../assets/logo.svg'
 
 const Footer = () => {
   return (
-    <div>
-      
+    <div className='gpt3__footer section__padding'>
+      <div className='gpt3__footer-heading'>
+        <h1 className='gradient__text'>So its finally THE END</h1>
+
+      </div>
+      <div className='gpt3__footer-button'>
+        <p>Request Early Access</p>
+      </div>
+      <div className='gpt3__footer-links'>
+        <div className='gpt3__footer-links_logo'>
+          <img src={gpt3Logo} alt='logo' />
+          <p>Address goes here ig</p>
+        </div>
+        <div className='gpt3__footer-links_div'>
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+
+        </div>
+        <div className='gpt3__footer-links_div'>
+          <h4>Company</h4>
+          <p>Terms & Condition</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+
+        <div className='gpt3__footer-links_div'>
+          <h4>Get in touch</h4>
+          <p>Addressh goes here</p>
+          <p>Phone Number goes here</p>
+          <p>info@payme.net</p>
+        </div>
+      </div>
+      <div className='gpt3__footer-copyright'>
+      <p>© 2021 GPT-3. All rights reserved.</p>
+      </div>
     </div>
   )
 }
